e7ef7caa8a7292034e72e97c5c6d2890a88f10e0
Jordan <info.viralwork@gmail.com>
2017-01-02 11:25:23 -0600

    Add files via upload

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+# create multiresolution windows icon
+ICON_DST=../../src/qt/res/icons/Eryllium.ico
 
+convert ../../src/qt/res/icons/Eryllium-16.png ../../src/qt/res/icons/Eryllium-32.png ../../src/qt/res/icons/Eryllium-48.png ${ICON_DST}
